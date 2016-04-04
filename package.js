2480e8b302b0793364d19867b42121ee2293821b
Abigail Watson <awatson1978@gmail.com>
2016-04-03 23:04:21 -0700

    bump and publish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout-pagescreen-config',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Default pagescreen configuration for ActiveLayout. For testing and debugging, mostly.',
   git: 'https://github.com/clinical-meteor/active-layout-pagescreen-config',
   documentation: 'README.md'
@@ -10,7 +10,7 @@ Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
   api.use('meteor-platform');
-  api.use('clinical:active-layout@0.7.5');
+  api.use('clinical:active-layout@0.7.50');
   api.use('clinical:router@2.0.17');
 
   api.imply('clinical:active-layout');
